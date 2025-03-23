639056548f779f77cf3d93940cbd4b8b9b33fcb4
ynguyen2k2 <ynguyen2017tt@gmail.com>
2025-03-23 12:55:49 +0700

    add crud product image schema

---
+++ ClothingShop.Application/DTOs/ProductImageDto.cs
@@ -0,0 +1,11 @@
+﻿using ClothingShop.Core.Entities;
+
+namespace ClothingShop.Application.DTOs
+{
+    public class ProductImageDto:BaseDto
+    {
+        public string ImageUrl { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+    }
+}
