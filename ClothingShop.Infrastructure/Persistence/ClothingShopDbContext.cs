cb434e06ce9c3723213aaea99dc349ed6299735d
Huynh Van Thien <thienhuynh2605@gmail.com>
2025-03-23 14:29:48 +0700

    Add DbSet<ProductImage>

--- ClothingShop.Infrastructure/Persistence/ClothingShopDbContext.cs
+++ ClothingShop.Infrastructure/Persistence/ClothingShopDbContext.cs
@@ -15,6 +15,7 @@ namespace ClothingShop.Infrastructure.Persistence
         public DbSet<UserAccount> UsersAccounts { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductVariant> ProductVariants { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
