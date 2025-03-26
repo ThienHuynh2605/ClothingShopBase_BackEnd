6217bce8f24f3f6cbe4bde0c63b50e05009322dd
Huynh Van Thien <thienhuynh2605@gmail.com>
2025-03-26 11:27:22 +0700

    SCRUM-41-add-unit-of-work

---
+++ ClothingShop.Infrastructure/Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using ClothingShop.Core.Common;
+using ClothingShop.Infrastructure.IRepositories;
+using ClothingShop.Infrastructure.IRepositories.SpecificRepository;
+using ClothingShop.Infrastructure.IRepositories.UnitOfWork;
+using ClothingShop.Infrastructure.Persistence;
+using ClothingShop.Infrastructure.Repositories.SpecificRepository;
+
+namespace ClothingShop.Infrastructure.Repositories.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ClothingShopDbContext _context;
+
+        public IUserRepository Users { get; }
+        public ICartRepository Carts { get; }
+        public IProductRepository Products { get; }
+        public ICartProductRepository CartsProducts { get; }
+
+        public UnitOfWork(ClothingShopDbContext context)
+        {
+            _context = context;
+            Users = new UserRepository(context);
+            Carts = new CartRepository(context);
+            Products = new ProductRepository(context);
+            CartsProducts = new CartProductRepository(context);
+        }
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose() => _context.Dispose();
+    }
+}
